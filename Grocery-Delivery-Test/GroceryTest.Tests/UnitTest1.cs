2715c999c2f7898445a8f495ee0377614f25ffaa
Zachery Younggren <zwyounggren1@buffs.wtamu.edu>
2022-02-20 23:38:23 -0600

    Noerror

--- Grocery-Delivery-Test/GroceryTest.Tests/UnitTest1.cs
+++ Grocery-Delivery-Test/GroceryTest.Tests/UnitTest1.cs
@@ -1,17 +1,56 @@
-// using Xunit;
-// using GroceryDelivery;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GroceryDelivery;
+using Xunit;
 
-// namespace GroceryTest.Tests
-// {
-//     public class GroceryTest_IsPrimeShould
-//     {
-//         [Fact]
-//         public void IsPrime_InputIs1_ReturnFalse()
-//         {
-//             var groceryTest = new GroceryTest();
-//             bool result = groceryTest.IsPrime(1);
+namespace GroceryDelivery.Models
+{
+    public class DataAccessLayerTest
+    {
+        [Fact]
+        public async Task AddItemAsync_ItemIsAdded()
+        {
+            using (var db = new GroceryDbContext(Utilities.TestDbContextOptions()))
+            {
+                // Arrange
+                var recId = 10;
+                var expectedItem = new Item()
+                {
+                    ItemID = recId,
+                    ItemName = "New Item",
+                    ItemType = "Some Type",
+                    Quantity = 3,
+                    Price = 4.00
+                };
+                
+                // Act
+                await db.AddItemAsync(expectedItem);
 
-//             Assert.False(result, "I should not be prime");
-//         }
-//     } 
-// }
+                // Assert
+                var actualItem = await db.FindAsync<Item>(10);
+                Assert.Equal(expectedItem, actualItem);
+                
+            }
+        }
+
+        // [Fact]
+        // public async Task AddMessageAsync_MessageIsAdded()
+        // {
+        //     using (var db = new AppDbContext(Utilities.TestDbContextOptions()))
+        //     {
+        //         // Arrange
+        //         var recId = 10;
+        //         var expectedMessage = new Message() { Id = recId, Text = "Message" };
+
+        //         // Act
+        //         await db.AddMessageAsync(expectedMessage);
+
+        //         // Assert
+        //         var actualMessage = await db.FindAsync<Message>(recId);
+        //         Assert.Equal(expectedMessage, actualMessage);
+        //     }
+        // }
+    }
+}
